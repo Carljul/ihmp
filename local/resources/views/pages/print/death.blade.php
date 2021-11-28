1ed26b304fc8e22dbbf7b21f3257d15e120f742a
Julcarl Selma <julcarl.selma@takingyouforward.org>
2021-11-28 13:58:13 +0800

    [ADD] - New death certificate design

--- local/resources/views/pages/print/death.blade.php
+++ local/resources/views/pages/print/death.blade.php
@@ -35,212 +35,228 @@
         return date('F d, Y',strtotime($date));
     }
 @endphp
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$content->firstname}} {{$content->middlename}} {{$content->lastname}} {{$content->suffix ?? ''}}</title>
-    <link rel="stylesheet" href="{{ asset('css/materialize.min.css') }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ setting('site.title') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+
+    <!--Import Google Icon Font-->
+    <link href="{{ asset('css/icon.css') }}" rel="stylesheet">
+
+    <!-- Tab Icon -->
+    <link href="{{ asset('storage/') }}/{{setting('site.logo')}}" rel="icon">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/materialize.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/ihmp.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/datatables.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/modal_flat.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/modal_rounded.css') }}" rel="stylesheet">
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
 
     <style>
-        .priest{
-            position: absolute;
-            bottom: 7%;
-            left: 40%;
-        }
-        table.custom-table{
-            width: 30%;
-            position: absolute;
-            left: 14% !important;
+
+        /* This is my main site's font */
+        @font-face {
+            font-family: 'GreatVibes';
+            src: url("{{asset('fonts/GreatVibes-Regular.ttf')}}")
         }
 
-        table.custom-table td{
-            padding: 0px !important;
+        @font-face {
+            font-family: 'Anton-Regular';
+            src: url("{{asset('fonts/Anton-Regular.ttf')}}")
         }
 
-        .bold {
+        @font-face {
+            font-family: 'LiberBaskerVille-Bold';
+            src: url("{{asset('fonts/LiberBaskerVille-Bold.ttf')}}")
+        }
+
+        @font-face {
+            font-family: 'LibreBaskerville-Italic';
+            src: url("{{asset('fonts/LibreBaskerville-Italic.ttf')}}")
+        }
+
+        @font-face {
+            font-family: 'LibreBaskerville-Regular';
+            src: url("{{asset('fonts/LibreBaskerville-Regular.ttf')}}")
+        }
+
+        @font-face {
+            font-family: 'Balgin-Light';
+            src: url("{{asset('fonts/Balgin-Light.ttf')}}")
+        }
+        @font-face {
+            font-family: 'OpenSans';
+            src: url("{{asset('fonts/OpenSans-VariableFont_wdth,wght.ttf')}}")
+        }
+        @media print{
+            * {
+                -webkit-print-color-adjust: exact;
+            }
+        }
+        .backgroundImage {
+            background: url("{{asset('storage/bg/certificates/death.png')}}");
+            background-size: cover;
+            background-repeat: no-repeat;
+            height: 100vh;
+        }
+        .toFade {
+            width: 100%;
+            height: 100vh;
+            opacity: 0.3;
+            position: absolute;
+            top: 0;
+        }
+        .r-m-t {
+            margin-top: 0;
+        }
+        .r-m-b {
+            margin-bottom: 0;
+        }
+        .certificate-content {
+            margin: 0 7%;
+        }
+        .line {
+            color: black;
+            border-bottom: 1px solid #20548b;
+        }
+        .line:before {
+            content: ':';
+            position: relative;
+            left: -17px;
+            bottom: -6px;
+        }
+        .rcaoc {
+            letter-spacing: 5px;
+            color: #6486aa;
+        }
+        .balgin{
+            font-family: 'Balgin-Light';
+        }
+        .asihm {
+            font-size: 20px;
+            letter-spacing: 6.4px;
+            font-family: 'Anton-Regular';
+        }
+        .mczip {
+            letter-spacing: 5px;
+        }
+        .contact {
+            font-size: 15px;
+        }
+        .death-color-theme {
+            color: #20548b;
+        }
+        .cob {
+            text-align: center;
+            font-size: 72px;
+            font-family: 'GreatVibes';
+            color: #004aad;
+        }
+        .certificate-content .s4 {
             font-weight: bold;
         }
-        .old-english{
-            font-family: "Euclid Fraktur", "Old English Text MT", fantasy, serif;
+        .certificate-content .s3 {
+            width: 28% !important;
         }
-
-        .underline{
-            text-decoration: underline;
-        }
-
-        body{
-            margin: 40px;
-        }
-        .border-top{
-            position: absolute;
-            margin-top: -136px;
-            margin-left: -120px;
-            height: 200px;
-        }
-
-        .border{
-            border-bottom: 1px solid black;
-            margin: 0px 10px;
-        }
-
-        .logo{
-            position: absolute;
-            left: 40%;
-            width: 150px;
-            bottom: 11%;
-        }
-
-        .classic-font{
-            font-family: Brush Script MT;
-        }
-        .marriage_top{
-            width: 300px;
-            position: absolute;
-            margin-top: -120px;
-        }
-        .death_bottom{
-            position: absolute;
-            width: 350px;
-            margin-left: -175px;
-            margin-top: -84px;
-            transform: scaleY(-1);
-        }
-        .death_top{
-            position: absolute;
-            width: 350px;
-            margin-left: -175px;
-            margin-top: -100px;
-        }
-
-        .f25{
-            font-size: 25px;
-        }
-        .marriage_side_top_left{
-            position: absolute;
-            left: 40px;
-            top: 40px;
-            width: 150px;
-        }
-
-        .marriage_side_top_right{
-            position: absolute;
-            right: 40px;
-            top: 40px;
-            transform: scaleX(-1);
-            width: 150px;
-        }
-        .marriage_side_bottom_left{
-            position: absolute;
-            left: 40px;
-            bottom: 40px;
-            transform: scaleY(-1);
-            width: 150px;
-        }
-        .marriage_side_bottom_right{
-            position: absolute;
-            right: 40px;
-            bottom: 40px;
-            transform: scaleX(-1) scaleY(-1);
-            width: 150px;
-        }
-        .churchBG{
-            width: 300px;
-            height: 300px;
-            position: absolute;
-            top: 200px;
-            left: 37%;
-            z-index: -1;
-            display: block;
-            margin-top: auto;
-            margin-bottom: auto;
-            opacity: 0.12;
-            border-radius: 1000px;
-        }
-        @page {
-            size: landscape;
-            margin: 12%;
+        .message {
+            font-weight: 500;
         }
     </style>
 </head>
 <body>
-    <div class="row">
-        <div class="col s12">
-            <center>
-                <img src="{{asset('storage/logo/marriage_sides.png')}}" alt="marriage_top" class="marriage_side_top_left">
-                <img src="{{asset('storage/logo/marriage_sides.png')}}" alt="marriage_top" class="marriage_side_top_right">
-                <img src="{{asset('storage/logo/marriage_sides.png')}}" alt="marriage_top" class="marriage_side_bottom_left">
-                <img src="{{asset('storage/logo/marriage_sides.png')}}" alt="marriage_top" class="marriage_side_bottom_right">
-                <img src="{{asset('storage/logo/death_border.png')}}" alt="death_top" class="death_top">
-                <h2 class="old-english">Certificate of Death</h2>
-                <img src="{{asset('storage/logo/death_border.png')}}" alt="death_bottom" class="death_bottom">
-                <br><br><br>
-                This is to acknowledge the death of
-                <br>
-                <h3 class="old-english">
-                    <span class="underline">&nbsp;&nbsp;&nbsp;{{$content->firstname}} {{$content->middlename}} {{$content->lastname}} {{$content->suffix}}&nbsp;&nbsp;&nbsp;</span>
-                </h3>
-                <br>
-                <div>
-                    On the <span class="bold underline">&nbsp;&nbsp;{{ordinal_suffix_of(deathGetDay($meta->date_of_death))}}&nbsp;&nbsp;</span>
-                    day of <span class="bold underline">&nbsp;&nbsp;{{month_string(deathGetMonth($meta->date_of_death))}}&nbsp;&nbsp;</span>
-                    In the year of <span class="bold underline">&nbsp;&nbsp;{{deathGetYear($meta->date_of_death)}}&nbsp;&nbsp;</span>
-                    with the age of <span class="bold underline">&nbsp;&nbsp;{{$meta->age}}&nbsp;&nbsp;</span>,
-                    a resident in <span class="bold underline">&nbsp;&nbsp;{{$meta->residence}}&nbsp;&nbsp;</span>
-                    with his/her Informant/Relatives <span class="bold underline">&nbsp;&nbsp;{{$meta->informant_or_relatives}}&nbsp;&nbsp;</span>.
-
-                    <br><br>
-                    Mr/Mrs/Ms. <span class="bold underline">&nbsp;&nbsp;{{$content->firstname}}&nbsp;&nbsp;</span> will be burried in
-                    <span class="bold underline">&nbsp;&nbsp;{{$meta->place_of_burial}}&nbsp;&nbsp;</span> on
-                    <span class="bold underline">&nbsp;&nbsp;{{deathDateGetFullStringFormat($meta->date_of_burial)}}&nbsp;&nbsp;</span>
-                </div>
-                <br>
-                <br>
-
-                <img src="{{asset('storage/logo/death_border.png')}}" alt="death_top" class="death_top">
-                <div>
-                    All data mentioned above appears in the Registry of Death at the Archdiocesan Shrine of the Immaculate Heart of Mary Parish;
-                    <br>
-                    <br>
-                    <table class="custom-table">
-                        <tr>
-                            <td>Book Number</td>
-                            <td>: <span class="bold">{{$meta->book_number}}</span></td>
-                        </tr>
-                        <tr>
-                            <td>Page Number</td>
-                            <td>: <span class="bold">{{$meta->page_number}}</span></td>
-                        </tr>
-                        <tr>
-                            <td>Registry Number</td>
-                            <td>: <span class="bold">{{$meta->registry_number}}</span></td>
-                        </tr>
-                        <tr>
-                            <td>Date Issued</td>
-                            <td>: <span class="bold">{{deathDateGetFullStringFormat($meta->date_issued)}}</span></td>
-                        </tr>
-                    </table>
-                </div>
-                <br>
-                <br>
-                <div class="priest">
-                    @if($content->priest_fname != null)
-                    <strong><span class="underline">&nbsp;&nbsp;{{$content->priest_clergy}} {{$content->priest_fname}} {{$content->priest_mname}} {{$content->priest_lname}} {{$content->priest_suffix}}&nbsp;&nbsp;</span></strong><br>
-                    @endif
-                    Parish Priest
-                </div>
-            </center>
+    <div class="container-fluid death-color-theme">
+        <div class="row center">
+            <div class="col s12 balgin">
+                <p class="r-m-b rcaoc">ROMAN CATHOLIC ARCHDIOCESE OF CEBU</p>
+                <p class="r-m-b r-m-t asihm">ARCHDIOCESAN SHRINE OF THE IMMACULATE HEART OF MARY</p>
+                <p class="r-m-b r-m-t mczip">Minglanilla, Cebu 6046</p>
+                <p class="r-m-b r-m-t contact">Tel. no. 260-3462/ Facebook page: https://www.facebook.com/ASIHM1857/</p>
+            </div>
         </div>
-        <img src="{{asset('storage/logo/ihm.jpg')}}" alt="background" class="churchBG">
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <h1 class="center r-m-t cob">Certificate of Death</h1>
+                    <p class="center message">This is to certify that the following data appears in the Registry of Death at the<br>Archdiocesan Shrine of the Immaculate Heart of Mary Parish:</p>
+                </div>
+            </div>
+            <div class="certificate-content">
+                <div class="row">
+                    <div class="col s4">NAME OF DECEASED</div>
+                    <div class="col s8 line">{{$content->firstname}} {{$content->middlename}} {{$content->lastname}} {{$content->suffix}}</div>
+                </div>
+                <div class="row">
+                    <div class="col s4">AGE</div>
+                    <div class="col s8 line">{{$meta->age}}</div>
+                </div>
+                <div class="row">
+                    <div class="col s4">RESIDENCE</div>
+                    <div class="col s8 line">{{$meta->residence}}</div>
+                </div>
+                <div class="row">
+                    <div class="col s4">DATE OF DEATH</div>
+                    <div class="col s8 line">{{ deathDateGetFullStringFormat(str_replace('//','/',$meta->date_of_death)) }}</div>
+                </div>
+                <div class="row">
+                    <div class="col s4">PLACE OF BURIAL</div>
+                    <div class="col s8 line">{{$meta->place_of_burial}}</div>
+                </div>
+                <div class="row">
+                    <div class="col s4">INFORMANT/RELATIVES</div>
+                    <div class="col s8 line">{{$meta->informant_or_relatives}}</div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col s4">BOOK NUMBER</div>
+                    <div class="col s3 line">{{$meta->book_number}}</div>
+                </div>
+                <div class="row">
+                    <div class="col s4">PAGE NUMBER</div>
+                    <div class="col s3 line">{{$meta->page_number}}</div>
+                </div>
+                <div class="row">
+                    <div class="col s4">REGISTRY NUMBER</div>
+                    <div class="col s3 line">{{$meta->registry_number}}</div>
+                </div>
+                <div class="row">
+                    <div class="col s4">DATE ISSUED</div>
+                    <div class="col s3 line">{{deathDateGetFullStringFormat(str_replace('//','/',$meta->date_issued))}}</div>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="col s6">
+                        <p>
+                            NOT VALID
+                            <br>
+                            WITHOUT
+                            <br>
+                            PARISH SEAL
+                        </p>
+                    </div>
+                    <div class="col s6" style="position: relative;">
+                        <div class="row center" style="position: absolute; right: 0; top: 25px;">
+                            <div class="col s12" style="border-bottom: 1px solid #20548b;">@if($content->priest_fname != null){{$content->priest_clergy}} {{$content->priest_fname}} {{$content->priest_mname}} {{$content->priest_lname}} {{$content->priest_suffix}}@endif</div>
+                            <div class="col s12">Parish Priest</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <script>
-        print();
-        setTimeout(function(){
-            location.href="/ihmp/certificate";
-        },1000);
-    </script>
+    <div class="toFade backgroundImage"></div>
 </body>
 </html>
